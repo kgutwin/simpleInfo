dc9ccc9e6af703119aad685ddfd12b59cae74fd3
kgutwin <karl@gutwin.org>
2014-01-25 20:33:39 -0800

    Weather updates working

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,73 +1,111 @@
-function iconFromWeatherId(weatherId) {
-  if (weatherId < 600) {
-    return 2;
-  } else if (weatherId < 700) {
-    return 3;
-  } else if (weatherId > 800) {
-    return 1;
-  } else {
-    return 0;
-  }
+var iconMap = {
+	'UNKNOWN': 0,
+	'clear-day': 1, 
+	'clear-night': 2, 
+	'rain': 3, 
+	'snow': 4, 
+	'sleet': 5, 
+	'wind': 6, 
+	'fog': 7, 
+	'cloudy': 8, 
+	'partly-cloudy-day': 9, 
+	'partly-cloudy-night': 10
+};
+
+var lastWeather = {
+	'icon': 0,
+	'temp': "",
+	'alerts': 0
+};
+
+function sendWeather(icon, temp, alerts) {
+	if (icon != lastWeather.icon || temp != lastWeather.temp || alerts != lastWeather.alerts) {
+		console.log("sendWeather chg " + icon + " " + temp + " " + alerts);
+		Pebble.sendAppMessage({
+			"wxCurIcon":icon,
+			"wxCurTemp":temp + "\u00B0",
+			"wxCurAlerts":alerts
+		});
+		lastWeather.icon = icon;
+		lastWeather.temp = temp;
+		lastWeather.alerts = alerts;
+	}
 }
 
 function fetchWeather(latitude, longitude) {
-  var response;
-  var req = new XMLHttpRequest();
-  req.open('GET', "http://api.openweathermap.org/data/2.1/find/city?" +
-    "lat=" + latitude + "&lon=" + longitude + "&cnt=1", true);
-  req.onload = function(e) {
-    if (req.readyState == 4) {
-      if(req.status == 200) {
-        console.log(req.responseText);
-        response = JSON.parse(req.responseText);
-        var temperature, icon;
-        if (response && response.list && response.list.length > 0) {
-          var weatherResult = response.list[0];
-          temperature = Math.round(weatherResult.main.temp - 273.15);
-          icon = iconFromWeatherId(weatherResult.weather[0].id);
-          console.log(temperature);
-          console.log(icon);
-          Pebble.sendAppMessage({
-            "WX_CUR_ICON":icon,
-            "WX_CUR_TEMP":temperature + "\u00B0C"});
-        }
-
-      } else {
-        console.log("Error");
-      }
-    }
-  };
-  req.send(null);
+	var response;
+	var req = new XMLHttpRequest();
+	req.open('GET', "http://api.forecast.io/forecast/1512d187338a135bfc8a2b5765c78e46/" +
+		latitude + "," + longitude + "?exclude=minutely,hourly,daily,flags", true);
+	req.onload = function(e) {
+		if (req.readyState == 4) {
+			if(req.status == 200) {
+				console.log(req.responseText);
+				response = JSON.parse(req.responseText);
+				var temperature, iconInt, alerts;
+				if (response && response.currently) {
+					var weatherResult = response.currently;
+					temperature = Math.round(weatherResult.temperature);
+					if (weatherResult.icon in iconMap) {
+						iconInt = iconMap[weatherResult.icon];
+					} else {
+						iconInt = 0;
+					}
+					console.log(temperature);
+					console.log(iconInt);
+					if (response.alerts) {
+						alerts = response.alerts.length;
+					} else {
+						alerts = 0;
+					}
+					sendWeather(iconInt, temperature, alerts);
+				}
+			} else {
+				console.log("Error " + req.status);
+				sendWeather(0, "N/R", 0);
+			}
+		}
+	};
+	req.send(null);
 }
 
 function locationSuccess(pos) {
-  var coordinates = pos.coords;
-  fetchWeather(coordinates.latitude, coordinates.longitude);
+	var coordinates = pos.coords;
+	console.log("locationSuccess " + coordinates.latitude + "," + coordinates.longitude);
+	fetchWeather(coordinates.latitude, coordinates.longitude);
 }
 
 function locationError(err) {
-  console.warn('location error (' + err.code + '): ' + err.message);
-  Pebble.sendAppMessage({
-    "WX_CUR_TEMP":"N/L"
-  });
+	console.warn('location error (' + err.code + '): ' + err.message);
+	sendWeather(0, "N/L", 0);
 }
 
 var locationOptions = { "timeout": 15000, "maximumAge": 60000 }; 
 
+var weatherIter = 0;
+function tryWeather(iter) {
+	console.log("tryWeather iter " + iter);
+	if (iter === 0) iter = weatherIter;
+	if (iter == weatherIter) {
+		navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+		setTimeout(function() { tryWeather( iter+1 ); }, 120000);
+		weatherIter = iter + 1;
+	}
+}
 
 Pebble.addEventListener(
 	"ready",
 	function(e) {
 		console.log("connect!" + e.ready);
-		console.log(e.type);
 	});
 
 Pebble.addEventListener(
 	"appmessage",
 	function(e) {
-		navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-		console.log(e.type);
-		console.log(e.payload.temperature);
 		console.log("message!");
+		console.log(JSON.stringify(e));
+		if (e.payload.wxGet) {
+			tryWeather(0);
+		}
 	});
 
--- src/main.c
+++ src/main.c
@@ -7,9 +7,37 @@ Layer *line_layer;
 
 GBitmap *wx_image;
 BitmapLayer *wx_image_layer;
+uint8_t wx_image_layer_animate;
 TextLayer *text_temp_layer;
 
 uint8_t snooze_ticks_remain;
+
+static AppSync sync;
+static uint8_t sync_buffer[64];
+
+AppTimer *resync_timer;
+#define RESYNC_TIMEOUT_MS (10 * 60 * 1000)
+
+enum {
+	MSG_WXCURTEMP = 1,
+	MSG_WXCURICON = 2,
+	MSG_WXCURALERTS = 3,
+	MSG_WXGET = 4,
+};
+
+uint8_t wx_image_map[] = {
+	RESOURCE_ID_IMAGE_WX_UNKNOWN,
+	RESOURCE_ID_IMAGE_WX_CLEAR_DAY,
+	RESOURCE_ID_IMAGE_WX_CLEAR_NIGHT,
+	RESOURCE_ID_IMAGE_WX_RAIN,
+	RESOURCE_ID_IMAGE_WX_SNOW,
+	RESOURCE_ID_IMAGE_WX_SLEET,
+	RESOURCE_ID_IMAGE_WX_WIND,
+	RESOURCE_ID_IMAGE_WX_FOG,
+	RESOURCE_ID_IMAGE_WX_CLOUDY,
+	RESOURCE_ID_IMAGE_WX_PARTLY_CLOUDY_DAY,
+	RESOURCE_ID_IMAGE_WX_PARTLY_CLOUDY_NIGHT
+};
 
 void line_layer_update_callback(Layer *layer, GContext* ctx) {
 	graphics_context_set_fill_color(ctx, GColorWhite);
@@ -62,14 +90,12 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
-	int frame = tick_time->tm_sec % 10;
-	
-	wx_image->bounds.origin.x = frame * 40;
-	//APP_LOG(APP_LOG_LEVEL_DEBUG, "Bounds: %d %d %d %d", wx_image->bounds.origin.x, wx_image->bounds.origin.y, 
-	//		wx_image->bounds.size.w, wx_image->bounds.size.h);
-	bitmap_layer_set_bitmap(wx_image_layer, wx_image);
-	Layer *wx_image_layer_layer = bitmap_layer_get_layer(wx_image_layer);
-	layer_mark_dirty(wx_image_layer_layer);
+	if (wx_image_layer_animate) {
+		wx_image->bounds.origin.x = (tick_time->tm_sec % 10) * 40;
+		bitmap_layer_set_bitmap(wx_image_layer, wx_image);
+		Layer *wx_image_layer_layer = bitmap_layer_get_layer(wx_image_layer);
+		layer_mark_dirty(wx_image_layer_layer);
+	}
 	
 	if (tick_time->tm_sec <= 1) {
 		handle_minute_tick(tick_time, units_changed);
@@ -79,18 +105,15 @@ void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 	if (snooze_ticks_remain == 0) {
 		if (tick_time->tm_min == 0 && tick_time->tm_sec <= 1) {
 			if (tick_time->tm_hour == 0) {
-				//APP_LOG(APP_LOG_LEVEL_DEBUG, "Snooze");
 				tick_timer_service_unsubscribe();
 				tick_timer_service_subscribe(MINUTE_UNIT, handle_second_tick);
 			} else if (tick_time->tm_hour == 6) {
-				//APP_LOG(APP_LOG_LEVEL_DEBUG, "Wake up");
 				tick_timer_service_unsubscribe();
 				tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 			}
 		}
 	} else if (snooze_ticks_remain == 1) {
 		if (tick_time->tm_hour < 6) {
-			//APP_LOG(APP_LOG_LEVEL_DEBUG, "Snooze timeout");
 			tick_timer_service_unsubscribe();
 			tick_timer_service_subscribe(MINUTE_UNIT, handle_second_tick);
 		}
@@ -134,13 +157,7 @@ void handle_init(void) {
 	layer_set_update_proc(line_layer, line_layer_update_callback);
 	layer_add_child(window_layer, line_layer);
 
-	wx_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WX_SNOW);
 	wx_image_layer = bitmap_layer_create(GRect(8, 18, 40, 40));
-	wx_image->bounds.origin.x = 0;
-	wx_image->bounds.size.w = 40;
-	//APP_LOG(APP_LOG_LEVEL_DEBUG, "Bounds: %d %d %d %d", wx_image->bounds.origin.x, wx_image->bounds.origin.y, 
-	//		wx_image->bounds.size.w, wx_image->bounds.size.h);
-	bitmap_layer_set_bitmap(wx_image_layer, wx_image);
 	bitmap_layer_set_alignment(wx_image_layer, GAlignCenter);
 	layer_add_child(window_layer, bitmap_layer_get_layer(wx_image_layer));
 	
@@ -148,7 +165,6 @@ void handle_init(void) {
 	text_layer_set_text_color(text_temp_layer, GColorWhite);
 	text_layer_set_background_color(text_temp_layer, GColorClear);
 	text_layer_set_font(text_temp_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
-	text_layer_set_text(text_temp_layer, "31°");
 	layer_add_child(window_layer, text_layer_get_layer(text_temp_layer));
 	
 	snooze_ticks_remain = 30;
@@ -157,11 +173,73 @@ void handle_init(void) {
 	handle_minute_tick(NULL, YEAR_UNIT);
 }
 
+void request_wx_update() {
+	Tuplet command_tuple = TupletInteger(MSG_WXGET, 1);
+        
+	DictionaryIterator *iter;
+	app_message_outbox_begin(&iter);
+
+	if (iter == NULL) return;
+
+	dict_write_tuplet(iter, &command_tuple);
+	dict_write_end(iter);
+
+	app_message_outbox_send();
+}
+
+void resync_timer_handler(void *data) {
+	request_wx_update();
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+	app_timer_reschedule(resync_timer, RESYNC_TIMEOUT_MS);
+	switch (key) {
+		case MSG_WXCURICON:
+			if (wx_image) gbitmap_destroy(wx_image);
+			wx_image = gbitmap_create_with_resource(wx_image_map[new_tuple->value->uint8]);
+			wx_image->bounds.origin.x = 0;
+			wx_image->bounds.size.w = 40;
+			bitmap_layer_set_bitmap(wx_image_layer, wx_image);
+			wx_image_layer_animate = (new_tuple->value->uint8 > 0);	
+			break;
+		
+		case MSG_WXCURTEMP:
+			text_layer_set_text(text_temp_layer, new_tuple->value->cstring);
+			break;
+	}
+}
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+
+void app_message_init() {
+	Tuplet initial_values[] = {
+		TupletInteger(MSG_WXCURICON, 0),
+		TupletCString(MSG_WXCURTEMP, "--°"),
+		TupletInteger(MSG_WXCURALERTS, 0)
+	};
+	
+	app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values), 
+		sync_tuple_changed_callback, sync_error_callback, NULL);
+	// init buffers
+	app_message_open(64, 64);
+	// send initial weather request
+	request_wx_update();
+	resync_timer = app_timer_register(RESYNC_TIMEOUT_MS, resync_timer_handler, NULL);
+}
+
+void app_message_deinit() {
+	app_sync_deinit(&sync);
+}
 
 int main(void) {
 	handle_init();
+	app_message_init();
 
 	app_event_loop();
   
+	app_message_deinit();
 	handle_deinit();
 }
